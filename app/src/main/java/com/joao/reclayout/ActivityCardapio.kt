48797baa7fafcc08f649de3b23700ec782b6b538
JoaoMarcos <juka_jms@hotmail.com>
2021-01-17 22:42:25 -0300

    Atividade Layout

---
+++ app/src/main/java/com/joao/reclayout/ActivityCardapio.kt
@@ -0,0 +1,11 @@
+package com.joao.reclayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCardapio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cardapio)
+    }
+}
---
+++ app/src/main/java/com/joao/reclayout/ActivityTeclado.kt
@@ -0,0 +1,11 @@
+package com.joao.reclayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTeclado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teclado)
+    }
+}
